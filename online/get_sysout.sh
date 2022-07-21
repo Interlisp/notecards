26828d4e44c92124618f3eb6846e0920b9601038
Frank Halasz <frank@halasz.org>
2022-07-21 00:27:41 +0000

    Removing online from main since it has been obsoleted by the interlisp/online repo

--- online/get_sysout.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget "https://drive.google.com/uc?id=1W0vYJ4Rgf2gei9zZbZqsZrQ0rVYyajuG&export=download"
-mv uc* NOTECARDS.SYSOUT
-
-
--- online/web-portal/prod/systemd/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-sudo cp ./notecards.service /lib/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable notecards.service
